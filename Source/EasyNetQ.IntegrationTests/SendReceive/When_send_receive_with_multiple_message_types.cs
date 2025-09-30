d7f8740679837beecb41366b346c9e9eb37c365d
Alexander Kucherov <Alexander.Kucherov@kaspersky.com>
2025-09-30 08:25:01 +0300

    fix SendReceiv tests

--- Source/EasyNetQ.IntegrationTests/SendReceive/When_send_receive_with_multiple_message_types.cs
+++ Source/EasyNetQ.IntegrationTests/SendReceive/When_send_receive_with_multiple_message_types.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace EasyNetQ.IntegrationTests.SendReceive;
 
 [Collection("RabbitMQ")]
-public class When_send_receive_multiple_message_types : IDisposable
+public class When_send_receive_multiple_message_types : IDisposable, IAsyncLifetime
 {
     private readonly ServiceProvider serviceProvider;
     private readonly IBus bus;
@@ -18,9 +18,16 @@ public class When_send_receive_multiple_message_types : IDisposable
         bus = serviceProvider.GetRequiredService<IBus>();
     }
 
+    public Task InitializeAsync() => Task.CompletedTask;
+
     public virtual void Dispose()
     {
         serviceProvider?.Dispose();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await serviceProvider.DisposeAsync();
     }
 
     private const int MessagesCount = 10;
